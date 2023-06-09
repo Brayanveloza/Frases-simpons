b94c9fb43a170f4c04c7699e7a08b837bab4d789
marvel-react <brayanvelozacruz@gmial.com>
2023-06-09 18:15:47 -0500

    proyecto

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+
+import styled from "styled-components";
+
+
+export default function Header (){
+    return (
+        <Title>
+            Las mejores frases de Los Simpson
+        </Title>
+
+    );
+
+}
+
+const Title = styled.h1`
+    color: blue;
+    text-align:center;
+
+    
+`;
